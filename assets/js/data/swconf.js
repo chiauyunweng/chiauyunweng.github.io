bf3b526cf92e80de4dda31754cc5e17819d769bb
chiauyunweng <chiauyunweng@users.noreply.github.com>
2025-07-06 15:00:51 +0000

    deploy: 985221f9667dfc2eef5fae46e11256561209c7f5

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1751814051',resources: [
+      '/assets/css/.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
